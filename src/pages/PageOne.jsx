05367ed10a9033c1d14cd80e1fcb80c1394895c2
Ramen Biswas <ramen@devsramen.com>
2025-04-07 21:36:27 +0600

    Update PageOne.jsx

--- src/pages/PageOne.jsx
+++ src/pages/PageOne.jsx
@@ -4,7 +4,9 @@ import MasterLayer from '../components/layers/masterLayer';
 const PageOne = () => {
     return (
         <MasterLayer>
-            test
+            <div className="w-full h-96 flex justify-center items-center">
+                <h1 className='text-9xl font-bold text-center mt-52 capitalize'>Welcome <br/> to <br/> Our Projects</h1>
+            </div>
         </MasterLayer>
     );
 };
