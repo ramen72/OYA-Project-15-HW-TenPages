d271aa37a45b2b7592323dc56e12149724df4cca
ramen72 <ramen@devsramen.com>
2025-04-07 14:51:08 +0600

    Update Project Nav section at 04-04-2025_1450.

--- src/data/NavItemData.jsx
+++ src/data/NavItemData.jsx
@@ -1,12 +1,12 @@
 export const navItemData = [
-    {
-        "itemText" : "DummyLink",
-        "url" : "https://www.okler.net/previews/porto/12.1.0/demo-business-consulting-2.html",
-    },
-    {
-        "itemText" : "Home",
-        "url" :  "https://preview.themeforest.net/item/porto-responsive-html5-template/full_screen_preview/4106987?_ga=2.110173125.1337380955.1742921267-617989526.1726215636"
-    },
+    // {
+    //     "itemText" : "DummyLink",
+    //     "url" : "https://www.okler.net/previews/porto/12.1.0/demo-resume-blue.html",
+    // },
+    // {
+    //     "itemText" : "Home",
+    //     "url" :  "https://preview.themeforest.net/item/porto-responsive-html5-template/full_screen_preview/4106987?_ga=2.110173125.1337380955.1742921267-617989526.1726215636"
+    // },
     {
         "itemText" : "Building dreams",
         "url" : "/projectOne",
@@ -15,14 +15,16 @@ export const navItemData = [
     {
         "itemText" : "INCREDIBLE DESIGNS",
         "url" : "/projectTwo",
-        "pageOneUr2" : "https://www.okler.net/previews/porto/12.1.0/index-corporate-8.html",
+        "pageOneUrl" : "https://www.okler.net/previews/porto/12.1.0/index-corporate-8.html",
     },
     {
         "itemText" : "Business Consulting",
-        "url" : "/businessConsulting"
+        "url" : "/businessConsulting",
+        "pageOneUrl":'https://www.okler.net/previews/porto/12.1.0/demo-business-consulting-2.html'
     },
-    {
-        "itemText" : "Page - 5",
-        "url" : "#"
-    },
+    // {
+    //     "itemText" : "Page - 5",
+    //     "url" : "#",
+    //     "pageOneUrl":"https://www.okler.net/previews/porto/12.1.0/demo-resume-blue.html"
+    // },
 ]
